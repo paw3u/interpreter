97a28c0a1eb00cad1140ebbd4f324d9a7353c70b
paw3u <you@example.com>
2025-12-11 20:47:51 +0100

    first sketch

---
+++ main.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+
+// ================ Lexer ================ //
+
+void next_token(lexer_t *lex) {
+    while(*lex->pos && isspace(*lex->pos)) lex->pos++;
+
+    lex->token.start = lex->pos;
+    lex->token.length = 1;
+
+    if(!*lex->pos) { lex->token.type = TK_EOF; return; }
+
+    char c = *lex->pos++;
+
+    if(isdigit(c)) {
+        lex->token.number = strtod(lex->token.start, &lex->pos);
+        lex->token.type = TK_NUM;
+        lex->token.length = lex->pos - lex->token.start;
+        return;
+    }
+    if(isalpha(c)) {
+        while(isalpha(*lex->pos)) lex->pos++;
+        lex->token.type = TK_ID;
+        lex->token.length = lex->pos - lex->token.start;
+        return;
+    }
+
+    lex->token.type = c;
+}
+
+lexer_t lexer(char *str) {
+    lexer_t lex;
+    lex.source = str;
+    lex.pos = lex.source;
+    //next_token(&lex);
+    return lex;
+}
+
+// ================ Parser ================ //
+
+node_handler_t node_handler[] = {
+    [TK_NUM]    = {node_num,    NULL,       0},
+    [TK_ID]     = {node_id,     NULL,       0},
+    [TK_LPAREN] = {node_group,  NULL,       0},
+    [TK_RPAREN] = {node_group,  NULL,       0},
+    [TK_PLUS]   = {NULL,        node_binop, 10},
+    [TK_MINUS]  = {NULL,        node_binop, 10},
+    [TK_MULT]   = {NULL,        node_binop, 20},
+    [TK_DIV]    = {NULL,        node_binop, 20},
+    [TK_POW]    = {NULL,        node_binop, 30},
+};
+
+node_t* node_alloc(lexer_t *lex, node_type_t type) {
+    node_t *node = (node_t*) calloc(1, sizeof(node_t));
+    node->type = type;
+    node->token_pos = lex->token.start - lex->source;
+    return node;
+}
+
+void node_free(node_t *node) {
+    while(node){
+        node_t *next = node->next;
+        node_free(node->child);
+        if(node->type == ND_ID) free(node->id);
+        free(node);
+        node = next;
+    }
+}
+
+node_t* node_group(lexer_t *lex) {
+    node_t *expr = node_expr(lex, 0);
+    if(lex->token.type != TK_RPAREN) {
+        fprintf(stderr, "')' missing at: %d\n", lex->token.start - lex->source);
+        return NULL;
+    }
+    return expr;
+}
+
+node_t* node_num(lexer_t *lex) {
+    node_t *node = node_alloc(lex, ND_NUM);
+    node->number = lex->token.number;
+    return node;
+}
+
+node_t* node_id(lexer_t *lex) {
+    node_t *node = node_alloc(lex, ND_ID);
+    char *name = (char*) malloc(lex->token.length + 1);
+    memcpy(name, lex->token.start, lex->token.length);
+    name[lex->token.length] = '\0';
+    return node;
+}
+
+node_t* node_binop(lexer_t *lex, node_t *left) {
+    token_type_t op = lex->token.type;
+    node_t *right = node_expr(lex, node_handler[op].lbp);
+    node_t *node = node_alloc(lex, ND_BINOP);
+    node->op = op;
+    node->child = left;
+    left->next = right;
+    return node;
+}
+
+node_t* node_unop(lexer_t *lex) {
+    node_t *node = node_alloc(lex, ND_UNOP);
+    return node;
+}
+
+node_t* node_call(lexer_t *lex) {
+    node_t *node = node_alloc(lex, ND_CALL);
+    return node;
+}
+
+node_t* node_expr(lexer_t *lex, int rbp) {
+    next_token(lex);
+    prefix_fun_t prefix = node_handler[lex->token.type].prefix;
+    node_t *node = prefix ? prefix(lex) : NULL;
+    if(!node){
+        fprintf(stderr, "Wrong token at: %d\n", lex->token.start - lex->source);
+        return NULL; // Tu chce coś innego niż exit...
+    }
+    next_token(lex);
+    while(rbp < node_handler[lex->token.type].lbp) {
+        infix_fun_t infix = node_handler[lex->token.type].infix;
+        node = infix ? infix(lex, node) : node;
+    }
+    return node;
+}
+
+node_t* parse(char *expr){
+    lexer_t lex = lexer(expr);
+    return node_expr(&lex, 0);
+}
+
+void node_print(node_t *node, int indent){
+    if (!node) return;
+    for (int i = 0; i < indent; i++) printf("  ");
+    switch(node->type) {
+        case ND_NUM: printf("NUM %.2f\n", node->number); break;
+        case ND_ID:  printf("ID %s\n", node->id); break;
+        case ND_BINOP:
+            printf("BINOP %c\n", node->op);
+            node_print(node->child, indent + 1);
+            node_print(node->child->next, indent + 1);
+            break;
+    }
+}
+
+ 
+
+// --------------------- main ----------------------
+
+int main(int argc, char *argv[]){ 
+    
+    printf("Witam\n");
+
+    //char *expr = "1/(22*x+4)-2*(123-x)+sin(0.231)";
+    char *expr = "1/(/22*x+4)-2*(123-x)";
+
+    node_t *root = parse(expr);
+    node_print(root, 0);
+
+    return 0;
+
+    lexer_t lex = lexer(expr);
+    next_token(&lex);
+    while(lex.token.type != TK_EOF){
+        printf("[");
+        fwrite(lex.token.start, 1, lex.token.length, stdout);
+        printf("], ");
+        next_token(&lex);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,80 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// ================ Lexer ================ //
+
+typedef enum {
+    TK_EOF = 0,
+    TK_NUM,
+    TK_ID,
+    TK_PLUS = '+',
+    TK_MINUS = '-',
+    TK_MULT = '*',
+    TK_DIV = '/',
+    TK_POW = '^',
+    TK_LPAREN = '(',
+    TK_RPAREN = ')',
+} token_type_t;
+
+typedef struct {
+    token_type_t type;
+    char *start;
+    size_t length;
+    double number;
+} token_t;
+
+typedef struct {
+    char *source;
+    char *pos;
+    token_t token;
+} lexer_t;
+
+
+void next_token(lexer_t *lex);
+lexer_t lexer(char *str);
+
+// ================ Parser ================ //
+
+typedef enum {
+    ND_BINOP,
+    ND_UNOP,
+    ND_NUM,
+    ND_ID,
+    ND_CALL,
+} node_type_t;
+
+typedef struct node_t node_t;
+
+struct node_t {
+    node_type_t type;
+    int token_pos;
+    node_t *child;
+    node_t *next;
+    union{
+        token_type_t op;
+        double number;
+        char *id;
+    };
+};
+
+typedef node_t* (*prefix_fun_t)(lexer_t *lex);
+typedef node_t* (*infix_fun_t)(lexer_t *lex, node_t *node);
+
+typedef struct {
+    prefix_fun_t prefix;
+    infix_fun_t infix;
+    int lbp;
+} node_handler_t;
+
+node_t* node_alloc(lexer_t *lex, node_type_t type);
+void node_free(node_t *node) ;
+node_t* node_group(lexer_t *lex);
+node_t* node_num(lexer_t *lex);
+node_t* node_id(lexer_t *lex);
+node_t* node_binop(lexer_t *lex, node_t *left);
+node_t* node_unop(lexer_t *lex);
+node_t* node_call(lexer_t *lex);
+node_t* node_expr(lexer_t *lex, int rbp);
+node_t* parse(char *expr);
+
+#endif
